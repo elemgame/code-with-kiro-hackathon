ed5b948184c45a10867b4c008a94be15574a3754
Glazlk <94984964+Glazlk@users.noreply.github.com>
2025-09-01 06:07:29 +0300

    Battle Arena design

--- src/components/BattleComponent.tsx
+++ src/components/BattleComponent.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import {
-  ELEMENTS,
-  LOCATIONS,
-  canAffordLocation,
-  formatCooldownTime,
-  getAvailableMana,
-  getElementalCooldownRemaining,
-  getElementalData,
-  isElementalOnCooldown,
+    ELEMENTS,
+    LOCATIONS,
+    canAffordLocation,
+    formatCooldownTime,
+    getAvailableMana,
+    getElementalCooldownRemaining,
+    getElementalData,
+    isElementalOnCooldown,
 } from '../gameLogic';
 import { Element, ElementalRarity, GameState, Location } from '../types';
 
@@ -39,12 +39,6 @@ const BattleComponent: React.FC<BattleComponentProps> = ({
   const renderLocationSelection = () => {
     return (
       <div className='battle-section'>
-        <div className='mana-display'>
-          <div className='mana-label'>Your Mana</div>
-          <div className='mana-value'>
-            {getAvailableMana(player.mana, player.selectedLocation)}
-          </div>
-        </div>
 
         <div className='battle-header'>
           <div className='battle-icon'>🏟️</div>
@@ -93,12 +87,6 @@ const BattleComponent: React.FC<BattleComponentProps> = ({
 
     return (
       <div className='battle-section'>
-        <div className='mana-display'>
-          <div className='mana-label'>Your Mana</div>
-          <div className='mana-value'>
-            {getAvailableMana(player.mana, player.selectedLocation)}
-          </div>
-        </div>
 
         <div className='battle-navigation'>
           <button className='back-btn' onClick={onReturnToLocationSelection}>
